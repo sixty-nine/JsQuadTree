431a9096c699828395a1c412b567708555937b22
Daniele Barsotti <daniele.barsotti@virtua.ch>
2014-12-20 17:18:12 +0100

    Cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,3 @@
-//module.exports = function(grunt) {
-//    'use strict';
-//
-//    // Project configuration.
-//    grunt.initConfig({
-//        pkg: grunt.file.readJSON('package.json'),
-////        uglify: {
-////            options: {
-////                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-////            },
-////            build: {
-////                src: 'src/js/<%= pkg.name %>.js',
-////                dest: 'build/<%= pkg.name %>.min.js'
-////            }
-////        },
-//        jshint: {
-//            all    : ['src/**/*.js'],
-//            options: {
-//                jshintrc : '.jshintrc'
-//            }
-//        }
-//    });
-//
-//    // Load the plugin that provides the "uglify" task.
-//    grunt.loadNpmTasks('grunt-contrib-uglify');
-//
-//    grunt.loadNpmTasks('grunt-contrib-jshint');
-//
-//    // Default task(s).
-//    grunt.registerTask('default', ['jshint']);
-//
-//
-//};
-
-
-
 module.exports = function (grunt) {
     'use strict';
 
