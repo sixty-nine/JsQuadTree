d2678acc361f2f44e10dc4e3938a064edd128ef4
Daniele Barsotti <daniele.barsotti@virtua.ch>
2014-12-20 23:23:05 +0100

    More tests

--- test/unit/QuadTreeSpec.js
+++ test/unit/QuadTreeSpec.js
@@ -8,6 +8,83 @@ define(function (require) {
 
     describe('model/QuadTree', function () {
 
+        it('constructs and fills properly', function () {
+
+            var p1 = new Point(1, 1),
+                p2 = new Point(9, 1),
+                p3 = new Point(9, 9),
+                p4 = new Point(1, 9),
+                bb = new BoundingBox(5, 5, 5),
+                tree = new QuadTree(bb);
+
+            expect(tree.boundaries).toBeDefined();
+            expect(tree.isDivided).toBeDefined();
+            expect(tree.children).toBeDefined();
+            expect(tree.points).toBeDefined();
+
+            expect(tree.boundaries).toBe(bb);
+            expect(tree.isDivided).toBe(false);
+            expect(tree.children).toEqual([]);
+            expect(tree.points).toEqual([]);
+
+            tree.insert(p1);
+            expect(tree.isDivided).toBe(false);
+            expect(tree.points).toEqual([p1]);
+
+            tree.insert(p2);
+            expect(tree.isDivided).toBe(false);
+            expect(tree.points).toEqual([p1, p2]);
+
+            tree.insert(p3);
+            expect(tree.isDivided).toBe(false);
+            expect(tree.points).toEqual([p1, p2, p3]);
+
+            tree.insert(p4);
+            expect(tree.isDivided).toBe(false);
+            expect(tree.points).toEqual([p1, p2, p3, p4]);
+
+            // -- After the 5th point the tree splits
+            tree.insert(p1);
+            expect(tree.isDivided).toBe(true);
+            expect(tree.points).toEqual([]);
+            expect(tree.children).toEqual(jasmine.any(Array));
+            expect(tree.children.length).toBe(4);
+            _.each(tree.children, function (subtree) {
+                expect(subtree).toEqual(jasmine.any(QuadTree));
+                expect(subtree.isDivided).toBe(false);
+            });
+            expect(tree.children[0].points).toEqual([p1, p1]);
+        });
+
+        it('will not split indefinitely when the same point is inserted 5 times', function () {
+
+            function getMaxDepth(tree) {
+                var depth = 0,
+                    maxDepth = 0;
+
+                function walk(node) {
+                    maxDepth = Math.max(maxDepth, depth);
+                    depth++;
+                    _.each(node.children, walk);
+                    depth--;
+                }
+                walk(tree);
+                return maxDepth;
+            };
+
+            var p = new Point(1, 1),
+                bb = new BoundingBox(5, 5, 5),
+                tree = new QuadTree(bb);
+
+            tree.insert(p);
+            tree.insert(p);
+            tree.insert(p);
+            tree.insert(p);
+            tree.insert(p);
+
+            expect(getMaxDepth(tree)).toBe(50);
+        });
+
         it('can be queried properly', function () {
 
             var tree = new QuadTree(new BoundingBox(5, 5, 5)),
