bde351f8c725b17a8acb334f804e492858ffd152
Daniele Barsotti <daniele.barsotti@virtua.ch>
2014-12-21 10:06:57 +0100

    Fixed the production build process + added dist/ folder to GIT

--- build/tasks/options/copy.js
+++ build/tasks/options/copy.js
@@ -2,6 +2,7 @@ module.exports = {
     production: {
         files: [
             { expand: true, cwd : 'src/static_files/', src: ['**'], dest: 'dist/static_files/' }, // includes files in path and its subdirs
+            { expand: true, cwd: 'src/bower_components/bootstrap/dist/css/', src: ['bootstrap.min.css', 'bootstrap.css.map'], dest: 'dist/css'},
         ]
     }
 };
